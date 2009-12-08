c379e61f090a94976a96267f25927c7672930aec
jan.mithrandir <jan.mithrandir@0678a0ba-2391-11de-85c7-557234b16fc6>
2009-12-08 09:03:29 +0000

    Tweaks to gallery

--- trunk/app/Gallery/Node.php
+++ trunk/app/Gallery/Node.php
@@ -10,16 +10,4 @@
  */
 class Gallery_Node extends Zend_Db_Table_Row_Abstract {
 
-    /**
-     * Get string representation of object
-     *
-     * @return string
-     */
-    function __toString() {
-        $ret = array();
-        foreach ($this->_data as $key => $value) {
-            $ret[] = $key." : ".$value;
-        }
-        return implode(' ', $ret);
-    }
 }
