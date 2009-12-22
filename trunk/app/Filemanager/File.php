aa97bac4f00938fbda6ee25d23d4a2a1fb587713
jan.mithrandir <jan.mithrandir@0678a0ba-2391-11de-85c7-557234b16fc6>
2009-12-22 09:25:10 +0000

    Launch preparations - contextmenus in file browser

--- trunk/app/Filemanager/File.php
+++ trunk/app/Filemanager/File.php
@@ -52,4 +52,65 @@ class Filemanager_File extends Zend_Db_Table_Row_Abstract {
     function isImage() {
         return substr($this->mimetype, 0, 5) === "image";
     }
+    
+    /**
+     * Get path to a thumbnail for an image
+     * Creates thumbnail if it does not exist, maintaining aspect ratio
+     * 
+     * @param int $h height of thumbnail
+     * @param int $w width of thumbnail
+     * @param bool $max whether to use the height and width as maximum values, otherwise it will be minimum
+     * @return string path to physical file
+     */
+    function getThumbnail($w = 1024, $h = 1024, $max = true) {
+    	if (!$this->isImage()) {
+    		return $this->getPath();
+    	}
+		// Get new dimensions
+		list ( $width_orig, $height_orig ) = getimagesize ( $this->getPath () );
+		
+		$ratio_orig = $width_orig / $height_orig;
+		
+		if ($max) {
+			if ($w / $h > $ratio_orig) {
+				$w = $h * $ratio_orig;
+			} else {
+				$h = $w / $ratio_orig;
+			}
+		} else {
+			if ($w / $h > $ratio_orig) {
+				$h = $h / $ratio_orig;
+			} else {
+				$w = $w * $ratio_orig;
+			}
+		}
+		
+		$thumbpath = ZfApplication::$_data_path . DIRECTORY_SEPARATOR . "files" . DIRECTORY_SEPARATOR . "thumb" . DIRECTORY_SEPARATOR . $this->nid . "_" . intval ( $w ) . "_" . intval ( $h ) . ($max ? "_max" : "");
+		if (! file_exists ( $thumbpath )) {
+			// Resample
+			$image_p = imagecreatetruecolor ( $w, $h );
+			switch ($this->mimetype) {
+				case "image/jpeg" :
+					$image = imagecreatefromjpeg ( $this->getPath () );
+					break;
+				
+				case "image/png" :
+					$image = imagecreatefrompng ( $this->getPath () );
+					break;
+				
+				case "image/wbmp" :
+					$image = imagecreatefromwbmp ( $this->getPath () );
+					break;
+				
+				case "image/gif" :
+					$image = imagecreatefromgif ( $this->getPath () );
+					break;
+			}
+			imagecopyresampled ( $image_p, $image, 0, 0, 0, 0, $w, $h, $width_orig, $height_orig );
+			
+			// Write to file
+			imagepng ( $image_p, $thumbpath, 0 );
+		}
+		return $thumbpath;
+	}
 }
