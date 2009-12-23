8d187ae7bcb502d4777d72948c8a40a397bba93a
jan.mithrandir <jan.mithrandir@0678a0ba-2391-11de-85c7-557234b16fc6>
2009-12-23 19:41:49 +0000

    More bugfixes

--- trunk/app/Content/Block/Spotlight.php
+++ trunk/app/Content/Block/Spotlight.php
@@ -14,6 +14,7 @@ class Content_Block_Spotlight extends Zoo_Block_Abstract  {
     public $template = "spotlight";
     
     private $can_edit = false;
+    private $content_id = 0;
     
     /**
      * Retrieve galleries listing
@@ -22,7 +23,7 @@ class Content_Block_Spotlight extends Zoo_Block_Abstract  {
      */
     function getTemplateVars() {
     	$item = Zoo::getService('content')->getContent($this->getSelectOptions(), 0, 1);
-        return array('item' => array_shift($item), 'can_edit' => $this->can_edit);
+        return array('item' => array_shift($item), 'can_edit' => $this->can_edit, 'id' => $this->content_id);
     }
     
     /**
@@ -34,6 +35,7 @@ class Content_Block_Spotlight extends Zoo_Block_Abstract  {
     	$rowset = Zoo::getService('content')->fetchAll($select);
     	foreach ($rowset as $item) {
     		// Will only be one item
+    		$this->content_id = $item->id;
 	    	try {
 		        $this->can_edit = Zoo::getService('acl')->checkItemAccess($item, 'edit');
 	        }
