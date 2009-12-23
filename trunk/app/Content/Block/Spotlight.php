cf5f6c0a0224a6b2118edab9bad5769dd49d60b9
jan.mithrandir <jan.mithrandir@0678a0ba-2391-11de-85c7-557234b16fc6>
2009-12-23 19:25:14 +0000

    Small tweaks to spotlight block and IE7 fixes to gallery layout

--- trunk/app/Content/Block/Spotlight.php
+++ trunk/app/Content/Block/Spotlight.php
@@ -19,10 +19,45 @@ class Content_Block_Spotlight extends Zoo_Block_Abstract  {
      * @return array
      */
     function getTemplateVars() {
-    	$item = Zoo::getService('content')->getContent(array('active' => true,
-                                                             'nodetype' => 'content_node',
-    													  	 'viewtype' => 'Display',
-                                                             'render' => true), 0, 1);
-        return array('item' => array_shift($item));
+    	$item = Zoo::getService('content')->getContent($this->getSelectOptions(), 0, 1);
+    	$can_edit = false;
+    	try {
+	        $can_edit = Zoo::getService('acl')->checkItemAccess($item, 'edit');
+        }
+        catch (Zoo_Exception_Service $e) {
+        	// No acl service installed
+        }
+        return array('item' => array_shift($item), 'can_edit' => $can_edit);
+    }
+    
+    /**
+     * Get cache ID - differs depending on whether the user can edit the content or not
+     * @see library/Zoo/Block/Zoo_Block_Abstract#getCacheId()
+     */
+    function getCacheId() {
+    	$select = Zoo::getService('content')->getContentSelect($this->getSelectOptions(), 0, 1);
+    	$rowset = Zoo::getService('content')->fetchAll($select);
+    	foreach ($rowset as $item) {
+    		// Will only be one item
+    		$can_edit = "";
+	    	try {
+		        $can_edit = Zoo::getService('acl')->checkItemAccess($item, 'edit') ? "_edit" : "";
+	        }
+	        catch (Zoo_Exception_Service $e) {
+	        	// No acl service installed
+	        }
+    		return parent::getCacheId().$can_edit;
+    	}
+    }
+    
+    /**
+     * Get the options used for selecting the spotlight node
+     * @return array
+     */
+    private function getSelectOptions() {
+    	return array('active' => true,
+    				'nodetype' => 'content_node',
+    				'viewtype' => 'Display',
+    				'render' => true);
     }
 }
