6392957dda06d01eb577e4e771364c6e68a757d9
jan.mithrandir <jan.mithrandir@0678a0ba-2391-11de-85c7-557234b16fc6>
2010-01-17 14:41:43 +0000

    Renaming Views to views to conform with ZF's (rather weirdly inconsistent) naming convention

---
+++ app/Rewrite/Plugin/Path.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package Rewrite
+ * @subpackage Plugin
+ *
+ */
+
+/**
+ * @package Rewrite
+ * @subpackage Plugin
+ *
+ */
+class Rewrite_Plugin_Path extends Zend_Controller_Plugin_Abstract {
+	/**
+	 * Locate path alias node
+	 * @see library/Zend/Controller/Plugin/Zend_Controller_Plugin_Abstract#routeStartup($request)
+	 * 
+	 * @param Zend_Controller_Request_Abstract $request
+	 * @return void
+	 */
+	public function routeShutdown(Zend_Controller_Request_Abstract $request) {
+		$factory = new Rewrite_Path_Factory();
+    	$path = $factory->findPath($request->getRequestUri());
+    	if ($path) {
+	    	$content_service = Zoo::getService ( 'content' );
+			$request->setActionName($content_service->action);
+			$request->setControllerName($content_service->controller);
+			$request->setModuleName($content_service->module);
+			$request->setParam('id', $path->nid);
+		}
+	}
+}
