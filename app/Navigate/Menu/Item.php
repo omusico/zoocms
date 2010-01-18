0a725ebfb7d934e15da88ea4c0b90e609ea5481e
jan.mithrandir <jan.mithrandir@0678a0ba-2391-11de-85c7-557234b16fc6>
2010-01-18 14:54:53 +0000

    Navigation module skeleton added

---
+++ app/Navigate/Menu/Item.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package    Navigate
+ * @subpackage Menu
+ */
+
+/**
+ * @package    Navigate
+ * @subpackage Menu
+ * @copyright  Copyright (c) 2010 ZooCMS
+ * @version    1.0
+ */
+class Navigate_Menu_Item extends Zend_Db_Table_Row_Abstract {
+    /**
+     * id
+     * mid (menuid)
+     * pid (parent - another menu item)
+     * title
+     * destination (numeric = content node id, else url)
+     * target
+     * active
+     */
+
+}
---
+++ app/Navigate/Menu/Item/Factory.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package  Navigate
+ * @subpackage Menu
+ *
+ */
+
+/**
+ * @package  Navigate
+ * @subpackage Menu
+ *
+ */
+class Navigate_Menu_Item_Factory extends Zoo_Db_Table {
+}
