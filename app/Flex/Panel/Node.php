0467179a2d21d2d134ea84605482eb1c89c1d582
jan.mithrandir <jan.mithrandir@0678a0ba-2391-11de-85c7-557234b16fc6>
2011-02-06 00:59:04 +0000

    First iteration Panel implementation
    Discovery mechanism first attempt ready for testing

---
+++ app/Flex/Panel/Node.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * @package    Flex
+ * @subpackage Panel
+ */
+
+/**
+ * Flex_Panel_Node
+ *
+ * @package    Flex
+ * @subpackage Panel
+ * @copyright  Copyright (c) 2008 ZooCMS
+ * @version    1.0
+ */
+class Flex_Panel_Node extends Zend_Db_Table_Row_Abstract {
+  /**
+   * panel_id
+   * nid
+   * parent
+   * nodetype
+   */
+}
